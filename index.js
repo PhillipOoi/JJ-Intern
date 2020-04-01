0e0f50a3044625a32a15c7b7b5881d41c2e5e235
PhillipOoi <PhillipOoi@users.noreply.github.com>
2020-04-02 02:02:48 +0800

    Hapi framework

--- index.js
+++ index.js
@@ -1,43 +1,81 @@
 'use strict'
-require('env2')('.env')
+require('env2')('env.json')
 const Hapi = require('@hapi/hapi')
-const Path = require('path')
+const Pack = require('./package.json')
+var engine = require('hapi-react')();
 
 const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+    debug: { request: ['error'] }
+  })
 
-    const server = Hapi.server({
-        port: 8080,
-        host: 'localhost'
-    })
+  await server.register(require('hapi-auth-jwt2'))
+  server.auth.strategy('jwt', 'jwt', {
+    key: process.env.JWT_SECRET,
+    validate: require('./util/jwtValidate')
+  })
+  server.auth.default('jwt')
 
-    await server.register(require('hapi-auth-jwt2'))
+  await server.register(require('vision'), (err) => {
+    assert.ifError(err);
 
-    server.auth.strategy('jwt', 'jwt', {
-        key: process.env.JWT_SECRET,
-        validate: require('./util/jwtValidate')
-    })
+    server.views({
+      defaultExtension: 'jsx',
+      engines: {
+        jsx: engine, // support for .jsx files
+        js: engine // support for .js
+      },
+      relativeTo: __dirname,
+      path: 'views'
+    });
+  });
 
-    server.auth.default('jwt')
-
-    await server.register([{
-        plugin: require('hapi-pgsql'),
-        options: {
-            database_url: process.env.DATABASE_URL
-        }
+  const swaggerOptions = {
+    info: {
+      title: 'Todo API Documentation',
+      version: Pack.version
+    }
+  }
+  await server.register([
+    {
+      plugin: require('@hapi/inert')
     }, {
-        plugin: require('hapi-auto-route'),
-        options: {
-            routes_dir: Path.join(__dirname, 'routes')
-        }
-    }])
+      plugin: require('@hapi/vision')
+    }, {
+      plugin: require('hapi-pgsql'),
+      options: {
+        database_url: process.env.DATABASE_URL
+      }
+    }, {
+      plugin: require('hapi-router'),
+      options: {
+        routes: 'routes/*.js'
+      }
+    }, {
+      plugin: require('hapi-swagger'),
+      options: swaggerOptions
+    }, {
+      plugin: require('hapi-redis2'),
+      options: {
+        settings: 'redis://127.0.0.1:6379/2',
+        decorate: true
+      }
+    }
+  ])
 
-    await server.start()
-    console.log('Server running on %s', server.info.uri)
+  await server.start()
+  console.log('Server running on %s', server.info.uri)
+
+  server.events.on('log', (event, tags) => {
+    console.log(event)
+  });
 }
 
 process.on('unhandledRejection', (err) => {
-    console.log(err)
-    process.exit(1)
+  console.log(err)
+  process.exit(1)
 })
 
 init()
